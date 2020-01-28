2553d6aa31055fa28f111b0e7ad618b1dae24ead
Bmix <codxxx@gmail.com>
2020-01-28 21:19:38 +0300

    Merge branch 'develop' of https://github.com/bmixdev/java_part1 into develop
    # Conflicts:
    #	src/main/java/ru/bmixdev/Lesson02.java

--- src/main/java/ru/bmixdev/Lesson04.java
+++ src/main/java/ru/bmixdev/Lesson04.java
@@ -54,19 +54,88 @@ public class Lesson04 {
         System.out.println(sb);
     }
 
-    private static void movePlayer() {
-        System.out.print("Введите номер строки: "); int row = scanner.nextInt();
-        System.out.print("Введите номер столбца: "); int col = scanner.nextInt();
-        arrProgress[row-1][col-1] = 1;
+    private static int checkDirection(char axis, int x, int y) {
+        System.out.format("checkDirection(%c,%d,%d)\n",axis,x,y);
+        boolean res = false;
+        int curX = x; int curY = y; int score = 0; int direct;
+        int scoreTotal = 0;
+        int curValue;
+        //for (int i = 0; i < 2; i++) {
+          //  curX = x;
+           // curY = y;
+           // direct = i == 0 ? -1 : 1;
+            switch (axis) {
+                case ('X'): {
+                    direct = 1;
+                    curY = 0;
+                    while (true) {
+                        curY += direct;
+                        if ((curY < 0) || (curY == arrProgress[curX].length)) break;
+                        curValue = arrProgress[curX][curY];
+                      //  if (curValue == 0) score = 0;
+                      //  else score += curValue;
+                        scoreTotal += curValue;
+                    }
+                    break;
+                }
+                case ('Y'): {
+                    direct = 1;
+                    curX = 0;
+                    while (true) {
+                        curX += direct;
+                        if ((curX < 0) || (curX == arrProgress.length)) break;
+                        curValue = arrProgress[curX][curY];
+                        //if (curValue == 0) score = 0;
+                        //else if (score != 0) score += curValue;
+                        scoreTotal += curValue;
+                    }
+                    break;
+                }
+            }
+    //    }
+        System.out.printf("scoreTotal: %d\n", scoreTotal);
+        return score;
     }
 
+    private static boolean checkWin(int player, int x, int y) {
+        int scoreX = checkDirection('X', x, y);
+        int scoreY = checkDirection('Y', x, y);
+        int scoreToWin = sizeArea < 5 ? sizeArea : 4;
+        if (Math.abs(scoreX) == scoreToWin
+                || Math.abs(scoreY) == scoreToWin) {
+            System.out.printf("Игра окончена. Победил: %s\n", player == 1 ? "Игрок" : "Компьютер" );
+            return true;
+        }
+        return false;
+    }
+
+    private static int[] movePlayer() {
+        int row;
+        while (true) {
+            System.out.print("Введите номер строки: ");
+            row = scanner.nextInt();
+            if (row < arrProgress.length) break;
+        }
+        int col;
+        while (true) {
+            System.out.print("Введите номер столбца: ");
+            col = scanner.nextInt();
+            if (row < arrProgress[row].length) break;
+        }
+        arrProgress[row-1][col-1] = 1;
+        return new int[] {row - 1, col -1};
+    }
+
+    private static void loopGame() {
+        initGame();
+        while (true) {
+            int[] movePoint = movePlayer();
+            printArea();
+            if (checkWin(1, movePoint[0], movePoint[1])) break;
+        }
+    }
 
     public static void main(String[] args) {
-        initGame();
-      //  printAscii();
-        movePlayer();
-
-        printArea();
-
+       loopGame();
     }
 }
